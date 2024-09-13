c71e80cd053c778df1066e33b9be79097b196281
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-09-13 03:00:34 -0700

    getting lint to pass

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,8 +1,6 @@
-const { fromJS } = require('immutable');
+import { fromJS } from './node_modules/immutable/dist/immutable';
 
-function getImmutableObject(object) {
-    return fromJS(object);
-}
+const getImmutableObject = (object) => fromJS(object);
 
-module.exports = getImmutableObject;
+export default getImmutableObject;
 
